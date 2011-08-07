1420ae94766d67361ecf7b5e3f6efa49cc5bd671
Kokai Laszlo <kokai.laszlo@koolin.hu>
2011-08-07 21:10:53 +0200

    webform nyugta oldal testreszabas, idopontok view modosites (incomplete) menu formazas

---
+++ sites/all/themes/limmud/webform-confirmation-83.tpl.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * @file
+ * Customize confirmation screen after successful submission.
+ *
+ * This file may be renamed "webform-confirmation-[nid].tpl.php" to target a
+ * specific webform e-mail on your site. Or you can leave it
+ * "webform-confirmation.tpl.php" to affect all webform confirmations on your
+ * site.
+ *
+ * Available variables:
+ * - $node: The node object for this webform.
+ * - $confirmation_message: The confirmation message input by the webform author.
+ * - $sid: The unique submission ID of this submission.
+ */
+?>
+<?php 
+include_once(drupal_get_path('module', 'webform') .'/includes/webform.submissions.inc');
+kpr($node);
+$confirmation_message = str_replace("[%sid]", $sid, $confirmation_message);
+$submission = webform_get_submission($node->nid, $sid);
+kpr($submission);
+?>
+<div class="webform-confirmation">
+  <?php if ($confirmation_message): ?>
+    <?php print $confirmation_message ?>
+  <?php else: ?>
+    <p><?php print t('Thank you, your submission has been received.'); ?></p>
+  <?php endif; ?>
+</div>
+
+<div class="links">
+  <a href="<?php print url('node/'. $node->nid) ?>"><?php print t('Go back to the form') ?></a>
+</div>
