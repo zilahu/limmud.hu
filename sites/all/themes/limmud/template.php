f14add9426a41ae1137123bfe3664d69bff9c633
Kokai Laszlo <kokai.laszlo@koolin.hu>
2011-08-13 18:31:59 +0200

    Előadások adatlapján kategória linkek
    Előadások adatlapján a cimkék, termek és előadás típusok már évenként
    listázzák az előadásokat.

--- sites/all/themes/limmud/template.php
+++ sites/all/themes/limmud/template.php
@@ -39,3 +39,33 @@ function limmud_form_user_login_block_alter(&$form) {
     );  
   }
 }
+
+function limmud_preprocess_node(&$variables) {
+  $node = $variables['node'];
+
+  if ($node->type == 'konferencia_program' && $variables['view_mode'] == 'full') {
+    // field_presentation_location
+    // field_presentation_tags
+    // field_presentation_types
+    $node_year = substr($variables['field_presentation_time'][0]['value'], 0, 4);
+    
+    foreach ($variables['content']['field_presentation_location'] as $key => $value) {
+      if (is_numeric($key)) {
+        $variables['content']['field_presentation_location'][$key] = str_replace('taxonomy/term', 'konfesztival/'.$node_year, $variables['content']['field_presentation_location'][$key]);
+      }
+    }
+    foreach ($variables['content']['field_presentation_tags'] as $key => $value) {
+      if (is_numeric($key)) {
+        $variables['content']['field_presentation_tags'][$key] = str_replace('taxonomy/term', 'konfesztival/'.$node_year, $variables['content']['field_presentation_tags'][$key]);
+      }
+    }
+    foreach ($variables['content']['field_presentation_types'] as $key => $value) {
+      if (is_numeric($key)) {
+        $variables['content']['field_presentation_types'][$key] = str_replace('taxonomy/term', 'konfesztival/'.$node_year, $variables['content']['field_presentation_types'][$key]);
+      }
+    }
+    
+    // kpr($variables);
+  }
+  
+}
